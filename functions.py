c8dbe3cec1662dce32bbeca115592b6bf9428db6
Deadinside <kostyadevyatov@gmail.com>
2021-02-22 17:09:11 +0400

    all code refactor

---
+++ functions.py
@@ -0,0 +1,54 @@
+from vk_api import VkApi
+from vk_api.utils import get_random_id  # для get_random_id()
+import random
+import time
+
+
+def open_txt(txt_name):  # возвращяет текст из тестового файла
+    file = open("txt\\" + txt_name, "r", encoding="utf-8")  # txt\ папка где все лежит
+    output = file.read()
+    file.close()
+    return output
+
+
+accessToken = open_txt("access_token.txt")
+groupId = 181110264
+
+vkBotSession = VkApi(token=accessToken)
+vk = vkBotSession.get_api()
+
+
+def send_message(peer_id, message):  # отправляет сообщениие
+    vk.messages.send(
+        peer_id=peer_id,
+        message=message,
+        random_id=get_random_id(),
+    )
+
+
+def send_random(peer_id):  # возвращяет обращение к любому участнику беседы(нужны права администратора для бота)
+    answer = vk.messages.getConversationMembers(
+        peer_id=peer_id,
+    )
+    users = []
+    for i in answer["items"]:
+        users.append(i["member_id"])
+    return "@id" + str(random.choice(users))
+
+
+def psycho():
+    return " ".join([str(i) for i in range(1000, -1, -7)])
+
+
+def delay_send_message(peer_id, text_message, trigger):
+    global f
+    if not trigger:
+        send_message(peer_id, text_message)
+        f = time.perf_counter()
+    else:
+        s = time.perf_counter()
+        if s - f >= 600:
+            send_message(peer_id, text_message)
+            f = time.perf_counter()
+        else:
+            send_message(peer_id, "защита от манука")
---
+++ main.py
@@ -0,0 +1,45 @@
+from vk_api import VkApi
+from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
+import functions  # подключаем все функции
+
+accessToken = functions.open_txt("access_token.txt")
+groupId = 181110264
+
+vkBotSession = VkApi(token=accessToken)
+longPoll = VkBotLongPoll(vkBotSession, groupId)
+
+trigger = False
+
+for event in longPoll.listen():
+    if event.type == VkBotEventType.MESSAGE_NEW:
+        """
+        Если бот не отвечает на сообщения из беседы, то выдайте ему права
+        на чтение всей переписки, а лучше сразу права администратора.
+        """
+        from_id = event.obj["from_id"]  # id пользователя, который отправил сообщение
+        peer_id = event.obj["peer_id"]  # peer_id беседы или ЛС, откуда пришло сообщение
+
+        # lower - это метод приведения к нижнему регистру. Для регистронезависимости.
+        message = event.obj["text"].lower()
+
+        # message теперь в нижнем регистре, поэтому все проверки делаем тоже в нижнем регистре
+        if "zxc" in message:
+            functions.send_message(peer_id, "qwe")
+        elif "qwe" in message:
+            functions.send_message(peer_id, "zxc")
+        elif "призыв" in message:
+            functions.send_message(peer_id, "@all")
+        elif "ауе" in message:
+            functions.send_message(peer_id, functions.open_txt("aue.txt"))
+        elif "соня" in message:
+            functions.send_message(peer_id, "шлюха")
+        elif "пик пик пик" in message:
+            functions.delay_send_message(peer_id, functions.open_txt("ramzes.txt"), trigger)
+            trigger = True
+        elif "1000-7" in message:
+            functions.delay_send_message(peer_id, functions.psycho(), trigger)
+            trigger = True
+        elif "рандом" in message:
+            functions.send_message(peer_id, functions.send_random(peer_id))
+        elif "ping" in message:
+            functions.send_message(peer_id, "pong")
